ffd219a9b341d706b620b242c0246d3b2f9711a8
Paco Guerrer <fjgj1@hotmail.com>
2022-11-03 20:50:38 +0100

    Version 1.0

--- Tools/steamappsCleaner.sh
+++ Tools/steamappsCleaner.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+##############################################################################################################################################################
+# AUTOR: Paco Guerrero <fjgj1@hotmail.com>
+# ABOUT: Busca compatdata y shadercache en los dispositivos mostrando los nombres de los que se puedan utilizar y muesta como "Desconocido" los huérfanos.
+#           perfecto para eliminarlos.
+# REQUISITOS: Para una mejor salida de juego nonsteam se requiere Protontricks
+# SALIDAS:
+#   0: Todo correcto, llegamos al final.
+#   1: Si hemos pulsado el botón de "Salir" a mitad.
+#   2: Si salimos cancelando la eliminación
+##############################################################################################################################################################
+
+
+VERSION="1.0"
+RUTASEXTRA="/run/media /run/media/$USER"
+
 # Función para ejecutar asistente y borrar
 function lanzar() {
 
@@ -17,7 +32,7 @@ function lanzar() {
                 LISTAP+=("0" "$N" "$SALIDA" "$TAMANO")
             else
                 if SALIDASC=$(grep "$N" <"$IDSC"); then
-                    LISTAP+=("0" "$N" "${SALIDASC//$N}" "$TAMANO")
+                    LISTAP+=("0" "$N" "${SALIDASC//$N/}" "$TAMANO")
                 else
                     LISTAP+=("1" "$N" "Desconocido" "$TAMANO")
                 fi
@@ -39,7 +54,7 @@ function lanzar() {
         zenity --info \
             --title="SteamApps Cleaner" \
             --width=250 \
-            --text="Saliendo del programa.\nDisfruta tu Deck."
+            --text="Saliendo...\nDisfruta tu Deck o tu dispositivo Steam."
         salida
         exit 1
     fi
@@ -48,10 +63,10 @@ function lanzar() {
 
     if [ "${RUN}" ]; then
         zenity --question \
-            --title="Eliminar carpetas" --width=500 --height=200 \
+            --title="**** ATENCION - CUIDADO **** " --width=500 --height=200 \
             --ok-label="Eliminar y Continuar" \
             --cancel-label="Salir" \
-            --text="Eliminas los directorios con los siguientes IDs?\n${RUN}"
+            --text="Eliminas los directorios con los siguientes IDs?\n\n${RUN}\n\n\nRecuerda que estan ubicados en:\n${DIR}"
         ans=$?
         if [ ! $ans -eq 0 ]; then
             salida
@@ -67,28 +82,34 @@ function lanzar() {
 
 # Función para generar todos los IDs de Juegos
 function entrada() {
+
+    #Mostramos la versión
+    zenity --timeout 2 --info --text "Bienvenido a Steamapps Cleaner.\n\t\tVer: $VERSION" --width=300 --height=50
+
     IDPT=/tmp/PTsteamappsCleaner.tmp
     IDSC=/tmp/SCsteamappsCleaner.tmp
 
-    rm -rf "$IDPT" "$IDSC" 2> /dev/null
+    rm -rf "$IDPT" "$IDSC" 2>/dev/null
 
     #Generamos los IDs de protontricks
     flatpak run com.github.Matoking.protontricks -l 2>/dev/null >$IDPT
-    
+
     #Generamos los IDs de de los ficheros directamente
-    grep -n "name" "$HOME"/.steam/root/steamapps/*.acf 2>/dev/null \
-      | sed -e 's/^.*_//;s/\.acf:.:/ /;s/name//;s/"//g;s/\t//g;s/ /-/' | awk -F"-" '{printf "%-40s %s\n", $2, $1}' | sort | tee -a $IDSC >/dev/null
-        
-    LISTA=$(find /run/media -maxdepth 1 -mindepth 1 -type d)
-    for SD in $LISTA; do
-        grep -n "name" "$SD"/steamapps/*.acf 2>/dev/null \
-           | sed -e 's/^.*_//;s/\.acf:.:/ /;s/name//;s/"//g;s/\t//g;s/ /-/' | awk -F"-" '{printf "%-40s %s\n", $2, $1}' | sort | tee -a $IDSC >/dev/null
+    grep -n "name" "$HOME"/.steam/root/steamapps/*.acf 2>/dev/null |
+            sed -e 's/^.*_//;s/\.acf:.:/ /;s/name//;s/"//g;s/\t//g;s/ /-/' | awk -F"-" '{printf "%-40s %s\n", $2, $1}' | sort | tee -a $IDSC >/dev/null
+
+    for j in $RUTASEXTRA; do
+        LISTA=$(find "$j" -maxdepth 1 -mindepth 1 -type d)
+        for SD in $LISTA; do
+            grep -n "name" "$SD"/steamapps/*.acf 2>/dev/null |
+                sed -e 's/^.*_//;s/\.acf:.:/ /;s/name//;s/"//g;s/\t//g;s/ /-/' | awk -F"-" '{printf "%-40s %s\n", $2, $1}' | sort | tee -a $IDSC >/dev/null
+        done
     done
 }
 
 # Función para generar todos los IDs de Juegos
 function salida() {
-    rm -rf "$IDPT" "$IDSC" 2> /dev/null
+    rm -rf "$IDPT" "$IDSC" 2>/dev/null
 }
 
 #########################################
@@ -98,31 +119,33 @@ function salida() {
 entrada
 
 # Elimianmos los Compatdata
-DIR=/home/deck/.steam/root/steamapps/compatdata/
+DIR="$HOME/.steam/root/steamapps/compatdata/"
 TITLE="COMPATDATA"
 lanzar
 
 # Elimianmos los ShaderCache
-DIR=/home/deck/.steam/root/steamapps/shadercache/
+DIR="$HOME/.steam/root/steamapps/shadercache/"
 TITLE="SHADERCACHE"
 lanzar
 
-LISTA=$(find /run/media -maxdepth 1 -mindepth 1 -type d)
+for j in $RUTASEXTRA; do
+    LISTA=$(find "$j" -maxdepth 1 -mindepth 1 -type d)
 
-for SD in $LISTA; do
+    for SD in $LISTA; do
 
-    if [ -d "$SD/steamapps/compatdata/" ]; then
-        DIR="$SD/steamapps/compatdata/"
-        TITLE="COMPATDATA"
-        lanzar
-    fi
+        if [ -d "$SD/steamapps/compatdata/" ]; then
+            DIR="$SD/steamapps/compatdata/"
+            TITLE="COMPATDATA"
+            lanzar
+        fi
 
-    if [ -d "$SD/steamapps/shadercache/" ]; then
-        DIR="$SD/steamapps/shadercache/"
-        TITLE="SHADERCACHE"
-        lanzar
-    fi
+        if [ -d "$SD/steamapps/shadercache/" ]; then
+            DIR="$SD/steamapps/shadercache/"
+            TITLE="SHADERCACHE"
+            lanzar
+        fi
 
+    done
 done
 
 salida
