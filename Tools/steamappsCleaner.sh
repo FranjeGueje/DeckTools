f3d57b78b08ec774d7c6b5c99f0ab4b7fde62345
Paco Guerrer <fjgj1@hotmail.com>
2023-01-16 11:53:20 +0100

    Quitamos non-Steam por *

--- Tools/steamappsCleaner.sh
+++ Tools/steamappsCleaner.sh
@@ -23,7 +23,7 @@
 ##      VARIABLES GLOBALES
 #########################################
 # Versión de la aplicación
-VERSION="2.3"
+VERSION="2.3.1"
 # Nombre de la aplicación
 NOMBRE="Steamapps Cleaner"
 # Ruta de Steam principal, instalación local
@@ -130,11 +130,11 @@ function fRequisitos() {
     # Generamos los IDs de protontricks a la vez que comprobamos si tenemos protontricks
     if flatpak run com.github.Matoking.protontricks -l 2>/dev/null >$IDPT; then
         [ -n "$DEBUG" ] && echo "(log) protontricks encontrado en flatpak."
-        sed -i 's/team shortcut//' "$IDPT"
+        sed -i 's/Non-Steam shortcut: //' "$IDPT"
     else
         if protontricks -l 2>/dev/null >$IDPT; then
             [ -n "$DEBUG" ] && echo "(log) protontricks encontrado como aplicación."
-            sed -i 's/team shortcut//' "$IDPT"
+            sed -i 's/Non-Steam shortcut: //' "$IDPT"
         else
             zenity --timeout 10 --error --text "$lTEXTNOPROTON" --width=300 --height=50
             zenity --question --title="$lATENCION" --width=500 --height=200 --ok-label="$lCONTINLIMIT" --cancel-label="$lSALIR" --text="$lTEXTLIMIT"
@@ -232,7 +232,7 @@ function fPreparaSteamapps() {
 
                 if SALIDA=$(grep -w -m1 "$N" <"$IDPT"); then
                     SALIDA=$(echo "$SALIDA" | sed -E 's/\ \([0-9]+\)//g')
-                    LISTAP+=("0" "$i" "$N" "$SALIDA" "$TAMANO" "$DISCO" "$ORDERBYDISK" "${SUBDIR:0:6}" "N/A")
+                    LISTAP+=("0" "$i" "$N" "$SALIDA""*" "$TAMANO" "$DISCO" "$ORDERBYDISK" "${SUBDIR:0:6}" "N/A")
                     echo "$SALIDA" | tee "$NOMCACHE/$N.txt" >/dev/null
                 else
                     if SALIDASC=$(grep -w -m1 "$N" <"$IDSC"); then
